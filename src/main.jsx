630cf1dba0c8c68fd9ccbb598c3df0160fe99bd3
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-04-12 22:01:18 +0600

    added react helmet for dynamic page title

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 import "./index.css";
@@ -7,8 +8,10 @@ import AuthProvider from "./providers/AuthProvider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Home | Dwellify</title>
+      </Helmet>
       <p>This is Home page</p>
     </div>
   );

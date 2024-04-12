37e5504ad1972bb1c878b24b6152e4d18d094b61
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-04-13 01:06:44 +0600

    added Error Page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,26 @@
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="w-full min-h-fit h-screen p-20 font-poppins text-black">
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+      <div className="h-full text-center flex flex-col justify-center">
+        <div>
+          <h1 className="text-9xl">404</h1>
+          <h4 className="text-5xl font-light mt-4">Oops!</h4>
+          <p className="text-lg font-medium max-w-96 mx-auto mt-5 mb-3">
+            Looks like something went wrong and the page you are looking for can&apos;t be found.
+          </p>
+          <Link to="/" className="btn h-auto px-7 py-5 bg-black text-white hover:bg-black hover:text-white hover:translate-x-2">
+            Go Back
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("dfgdg");
+  const [user, setUser] = useState(null);
 
   const authInfo = {
     user,
